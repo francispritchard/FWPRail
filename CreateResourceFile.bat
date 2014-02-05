6b2eaf3119b6032b252d1ea1b6e2121158dd6206
FWP <francispritchard@fake.com>
2014-02-05 17:17:37 +0000

    Batch file to create a resource file for FWPRail

---
+++ CreateResourceFile.bat
@@ -0,0 +1,4 @@
+rem The resource file RailResource.rc has the location of the resources, so make sure it's up to date - fwp 1/2/14
+cd \Program Files (x86)\Embarcadero\RAD Studio\12.0\bin
+brcc32 -32 "C:\Doc\Google Drive\RAD Studio\Projects\Rail\RailResource.rc"
+pause
