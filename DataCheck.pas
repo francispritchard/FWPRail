2b2dee287586b5912694753a13b84ee275f90ee0
FWP <francispritchard@fake.com>
2014-09-02 18:42:35 +0100

    Further changes to general checking

--- DataCheck.pas
+++ DataCheck.pas
@@ -26,6 +26,9 @@ PROCEDURE CompareTwoPointDatabases(Point1DataFilename, Point1DataFilenameSuffix,
 PROCEDURE CompareTwoSignalDatabases(Signal1DataFilename, Signal1DataFilenameSuffix, Signal2DataFilename, Signal2DataFilenameSuffix : String);
 { Compare two signal databases - used for testing }
 
+PROCEDURE DoGeneralCheck;
+{ Debug check for various things including two different feedback units serving the same track circuit }
+
 PROCEDURE FormatCheckAllFiles;
 { Checks all .pas files for formatting errors }
 
@@ -36,7 +39,7 @@ IMPLEMENTATION
 
 {$R *.dfm}
 
-USES MiscUtils, StrUtils, InitVars, ADODB, Options;
+USES MiscUtils, StrUtils, InitVars, ADODB, Options, Raildraw, Feedback, CreateRoute;
 
 CONST
   UnitRef = 'TestUnit';
@@ -50,6 +53,304 @@ PROCEDURE Log(Str : String);
 BEGIN
   WriteToLogFile(Str + ' {UNIT=' + UnitRef + '}');
 END; { Log }
+
+PROCEDURE DoGeneralCheck;
+{ Debug check for various things including two different feedback units serving the same track circuit }
+CONST
+  IndicatorToBeSet = True;
+  SuppressMessage = True;
+  UpLine = True;
+
+VAR
+  ErrorCount : Integer;
+  FeedbackData : FeedbackRec;
+  FeedbackNum1 : Integer;
+  FeedbackNumFound : Boolean;
+  FeedbackPoint : Integer;
+  FeedbackType : TypeOfFeedBackType;
+  I, J : Integer;
+  L : Integer;
+  LenzPointNum : Integer;
+  LenzPointNumFound : Boolean;
+  LenzPointNumOutOfUse : Boolean;
+  P : Integer;
+  PointCount : Integer;
+  S : Integer;
+  SaveLine : Integer;
+  SaveTypeOfLine : TypeOfLine;
+  TC : Integer;
+  TCAboveFeedbackUnit : Integer;
+  TCHasFeedback : Boolean;
+  TCInUse : Boolean;
+  TempDraftRouteArray : StringArrayType;
+  TempLinesNotAvailableStr : String;
+
+BEGIN
+  DrawLineInLogFile(UnknownLocoChipAsZeroesStr, 'X', '=', UnitRef);
+  Log('X G E N E R A L_____C H E C K  {NOUNITREF}');
+  DrawLineInLogFile(UnknownLocoChipAsZeroesStr, 'X', '-', UnitRef);
+
+  WriteToStatusBarPanel(StatusBarPanel2, 'Feedback check');
+  ErrorCount := 0;
+  Debug('Commencing feedback unit check - please wait...');
+  FOR I := FirstFeedbackUnit TO LastFeedbackUnit DO BEGIN
+    FeedbackData.Feedback_Unit := I;
+    ExtractDataFromFeedback(FeedbackData, TCAboveFeedbackUnit, FeedbackType, FeedbackNum1);
+    IF FeedbackType = FeedbackDetectorOutOfUse THEN
+      Log('XG Feedback unit ' + IntToStr(FeedbackData.Feedback_Unit) + ' is marked as out of use');
+  END;
+
+  IF ErrorCount > 0 THEN
+    Debug('Feedback check completed - report in LogFile with header ''FFFF'' includes ' + IntToStr(ErrorCount) + ' errors found')
+  ELSE
+    Log('XG Feedback check completed - no feedback units serving more than one track circuit');
+
+  { Also check for line sections track circuit numbers unused }
+  ErrorCount := 0;
+  Debug('Commencing track circuit check - please wait...');
+  FOR TC := 0 TO High(TrackCircuits) DO BEGIN
+    TCInUse := False;
+    TCHasFeedback := False;
+
+    { see if the track circuit number is in use }
+    FOR L := 0 TO High(Lines) DO BEGIN
+      IF Lines[L].Line_TC = TC THEN
+        TCInUse := True;
+    END;
+
+    { now see if the track circuit has feedback }
+    FOR I := FirstFeedbackUnit TO LastFeedbackUnit DO BEGIN
+      FOR J := 1 TO 8 DO BEGIN
+        FeedbackData.Feedback_Unit := I;
+        FeedbackData.Feedback_Input := J;
+        ExtractDataFromFeedback(FeedbackData, TCAboveFeedbackUnit, FeedbackType, FeedbackNum1);
+        IF FeedbackType = TrackCircuitFeedbackDetector THEN BEGIN
+          IF FeedbackNum1 = TC THEN
+            TCHasFeedback := True;
+        END;
+      END;
+    END;
+
+      IF NOT TCInUse AND NOT TCHasFeedback THEN BEGIN
+        Inc(ErrorCount);
+        Log('X TC=' + IntToStr(TC) + ' not in use (and has no feedback)');
+      END ELSE
+        IF NOT TCInUse THEN BEGIN
+          Inc(ErrorCount);
+          Log('X TC=' + IntToStr(TC) + ' not in use');
+        END ELSE
+          IF NOT TCHasFeedback THEN BEGIN
+            Inc(ErrorCount);
+            Log('X TC=' + IntToStr(TC) + ' has no feedback');
+          END;
+  END;
+
+  IF ErrorCount > 0 THEN
+    Debug('Trackcircuit check completed - ' + IntToStr(ErrorCount) + ' errors noted in LogFile')
+  ELSE
+    Log('XG Trackcircuit check completed - all feedback inputs accounted for');
+
+  { Also check for track circuit feedback inputs unused }
+  ErrorCount := 0;
+  Debug('Commencing track circuit feedback input check - please wait...');
+  FOR I := FirstFeedbackUnit TO LastFeedbackUnit DO BEGIN
+    FOR J := 1 TO 8 DO BEGIN
+      FeedbackData.Feedback_Unit := I;
+      FeedbackData.Feedback_Input := J;
+      ExtractDataFromFeedback(FeedbackData, TCAboveFeedbackUnit, FeedbackType, FeedbackNum1);
+      IF FeedbackType = TrackCircuitFeedbackDetector THEN BEGIN
+        FeedbackNumFound := False;
+        FOR TC := 0 TO High(TrackCircuits) DO BEGIN
+          IF TC = FeedbackNum1 THEN BEGIN
+            FeedbackNumFound := True;
+
+            IF TrackCircuits[TC].TC_OccupationState = TCOutOfUseSetByUser THEN
+              Log('X Trackcircuit feedback unit ' + IntToStr(I) + ' input no. ' + IntToStr(J) + ' not in use as TC=' + IntToStr(TC) + ' is marked as being set out of use');
+          END;
+        END;
+
+        IF NOT FeedbackNumFound THEN BEGIN
+          Log('X Trackcircuit feedback unit ' + IntToStr(I) + ' input no. ' + IntToStr(J) + ' not in use ');
+          Inc(ErrorCount);
+        END;
+      END;
+    END;
+  END;
+
+  IF ErrorCount > 0 THEN
+    Debug('Trackcircuit feedback check completed - ' + IntToStr(ErrorCount) + ' errors noted in LogFile')
+  ELSE BEGIN
+    Log('XG Trackcircuit feedback check completed - all feedback inputs accounted for');
+  END;
+
+  { Debug point data }
+  ErrorCount := 0;
+  Debug('Commencing point feedback check - please wait...');
+  { now see if the point has feedback }
+  FOR P := 0 TO High(Points) DO BEGIN
+    IF NOT Points[P].Point_HasFeedback THEN BEGIN
+      Inc(ErrorCount);
+      Log('X Point ' + IntToStr(P) + ' has no feedback');
+    END;
+  END; {FOR}
+
+  IF ErrorCount > 0 THEN
+    Debug('Point check completed - ' + IntToStr(ErrorCount) + ' errors noted in LogFile')
+  ELSE BEGIN
+    Debug('Point check completed - all feedback inputs accounted for');
+    Log('X Point check completed - all feedback inputs accounted for');
+  END;
+
+  { Also check for point feedback inputs unused }
+  ErrorCount := 0;
+  Debug('Commencing point feedback input check - please wait...');
+  FOR I := FirstFeedbackUnit TO LastFeedbackUnit DO BEGIN
+    FOR J := 1 TO 8 DO BEGIN
+      FeedbackData.Feedback_Unit := I;
+      FeedbackData.Feedback_Input := J;
+      ExtractDataFromFeedback(FeedbackData, TCAboveFeedbackUnit, FeedbackType, FeedbackNum1);
+      IF FeedbackType = PointFeedbackDetector THEN BEGIN
+        FeedbackNumFound := False;
+        FeedbackPoint := UnknownPoint;
+        P := 0;
+        WHILE (P <= High(Points)) AND NOT FeedbackNumFound DO BEGIN
+          IF (Points[P].Point_FeedbackUnit = I) AND (Points[P].Point_FeedbackInput = J) THEN BEGIN
+            FeedbackNumFound := True;
+            FeedbackPoint := P;
+          END;
+
+          Inc(P);
+        END; {FOR}
+
+        IF NOT FeedbackNumFound THEN BEGIN
+          Log('X Point feedback unit ' + IntToStr(I) + ' input no. ' + IntToStr(J) + ' not used');
+          Inc(ErrorCount);
+        END ELSE
+          IF (FeedbackPoint <> UnknownPoint) AND Points[FeedbackPoint].Point_OutOfUse THEN
+            Log('X Point feedback unit ' + IntToStr(I) + ' input no. ' + IntToStr(J) + ' not in use as point is marked out of use');
+      END;
+    END; {FOR}
+  END; {FOR}
+
+  IF ErrorCount > 0 THEN
+    Debug('Point feedback unit check completed - ' + IntToStr(ErrorCount) + ' errors noted in LogFile')
+  ELSE
+    Log('X Point feedback unit check completed - all feedback inputs in use');
+
+  ErrorCount := 0;
+  { See if the same feedback unit is attached to two different points }
+  FOR I := 0 TO High(Points) DO BEGIN
+    FOR J := 0 TO High(Points) DO BEGIN
+      IF (I <> J)
+      AND (Points[I].Point_FeedbackUnit <> 0)
+      AND (Points[I].Point_FeedbackInput <> 0)
+      AND (Points[I].Point_FeedbackUnit = Points[J].Point_FeedbackUnit)
+      AND (Points[I].Point_FeedbackInput = Points[J].Point_FeedbackInput)
+      THEN BEGIN
+        Log('X Point ' + IntToStr(I) + ' and point ' + IntToStr(J) + ' have the same feedback input');
+        Inc(ErrorCount);
+      END;
+    END; {FOR}
+  END; {FOR}
+
+  IF ErrorCount > 0 THEN
+    Debug('Point feedback check completed - ' + IntToStr(ErrorCount) + ' errors noted in LogFile')
+  ELSE
+    Log('X Point feedback check completed - all feedback inputs accounted for');
+
+  { And show which points are not installed (by looking at the Lenz point numbers) }
+
+  { Need to count down to find the last Lenz point num }
+  ErrorCount := 0;
+  I := 1000;
+  PointCount := 0;
+  WHILE (I > 0) AND (PointCount = 0) DO BEGIN
+    J := 0;
+    WHILE J <= High(Points) DO BEGIN
+      IF Points[J].Point_LenzNum = I THEN
+        PointCount := I;
+      Inc(J);
+    END; {WHILE}
+    Dec(I);
+  END; {WHILE}
+
+  { Now record missing ones }
+  FOR I := 1 TO PointCount DO BEGIN
+    LenzPointNumFound := False;
+    LenzPointNumOutOfUse := False;
+    J := 0;
+    LenzPointNum := UnknownPoint;
+    WHILE J <= High(Points) DO BEGIN
+      IF Points[J].Point_LenzNum = I THEN BEGIN
+        LenzPointNum := J;
+        LenzPointNumFound := True;
+        IF Points[J].Point_OutOfUse THEN
+          LenzPointNumOutOfUse := True;
+      END;
+      Inc(J);
+    END; {WHILE}
+
+    IF NOT LenzPointNumFound THEN BEGIN
+      Log('X No Lenz point num ' + IntToStr(I));
+      Inc(ErrorCount);
+    END ELSE
+      IF LenzPointNumOutOfUse THEN
+        Log('X Lenz point num ' + IntToStr(I) + ' marked as out of use'
+               + ' [Unit ' + IntToStr(Points[LenzPointNum].Point_LenzUnit) + ' (' + Points[LenzPointNum].Point_LenzUnitType + ')]');
+  END;
+
+  IF ErrorCount > 0 THEN
+    Debug('Missing Lenz point numbers found - ' + IntToStr(ErrorCount) + ' errors noted in LogFile')
+  ELSE
+    Log('XG All Lenz point numbers accounted for');
+
+  { Show any signals which do not have resetting track circuits }
+  Debug('Checking for signal resetting track circuits...');
+  FOR S := 0 TO High(Signals) DO BEGIN
+    IF Signals[S].Signal_Type = SemaphoreDistant THEN
+      { semaphore distants don't have resetting track circuits }
+      Log('S [Note: Semaphore Distant S=' + IntToStr(S) + ' cannot have a resetting track circuit]')
+    ELSE BEGIN
+      IF NOT FindNextSignalOrBufferStop(S, UnknownSignal, UnknownBufferStop, NOT IndicatorToBeSet, TempLinesNotAvailableStr, TempDraftRouteArray) THEN BEGIN
+        IF NOT Signals[S].Signal_OutOfUse THEN
+          Log('S No resetting TC for S=' + IntToStr(S) + ' [' + LineToStr(Signals[S].Signal_AdjacentLine) + '] found as no next signal or bufferstop found')
+        ELSE
+          Log('S No resetting TC for S=' + IntToStr(S) + ' [' + LineToStr(Signals[S].Signal_AdjacentLine) + '] found as no next signal or bufferstop found but signal is '
+                 + 'marked as out of use');
+      END ELSE BEGIN
+        CreateLockingArrayFromDraftRouteArray(UnknownLocoChipStr, TempDraftRouteArray, Signals[S].Signal_RouteLockingNeededArray);
+        TC := GetResettingTrackCircuit(UnknownLocoChipStr, S, SuppressMessage);
+        IF TC = UnknownTrackCircuit THEN
+          Log('S No resetting TC for S=' + IntToStr(S) + ' [' + LineToStr(Signals[S].Signal_AdjacentLine) + '] found');
+      END;
+    END;
+  END; {FOR}
+
+  { See if any track circuits have different line characteristics, i.e. TC=126 occuping both UFYU5 (classified as a main/goods line) and FYX5 (classified as a fiddleyard).
+    This causes a problem in terms of speeds for track circuits.
+  }
+  SaveLine := UnknownLine;
+  Debug('Checking track circuit line types...');
+  FOR TC := 0 TO High(TrackCircuits) DO BEGIN
+    SaveTypeOfLine := UnknownTypeOfLine;
+    FOR L := 0 TO High(Lines) DO BEGIN
+      IF Lines[L].Line_TC = TC THEN
+        IF SaveTypeOfLine = UnknownTypeOfLine THEN BEGIN
+          SaveLine := L;
+          SaveTypeOfLine := Lines[L].Line_TypeOfLine;
+        END ELSE
+          IF Lines[L].Line_TypeOfLine <> SaveTypeOfLine THEN
+            Log('X TC=' + IntToStr(TC) + ' has line type of ' + TypeOfLineToStr(Lines[L].Line_TypeOfLine) + ' at ' + LineToStr(L)
+                   + ' but line type of ' + TypeOfLineToStr(SaveTypeOfLine) + ' at ' + LineToStr(SaveLine))
+          ELSE
+            SaveTypeOfLine := Lines[L].Line_TypeOfLine;
+    END; {FOR}
+  END; {FOR}
+
+  DrawLineInLogFile(UnknownLocoChipAsZeroesStr, 'X', '-', UnitRef);
+  Log('X E N D_____OF_____G E N E R A L_____C H E C K  {NOUNITREF}');
+  DrawLineInLogFile(UnknownLocoChipAsZeroesStr, 'X', '=', UnitRef);
+END; { DoGeneralCheck }
 
 PROCEDURE CompareTwoLineDatabases(Line1DataFilename, Line1DataFilenameSuffix, Line2DataFilename, Line2DataFilenameSuffix : String);
 { Compare two line databases - used for testing }
